570d54b24c123ccf250f570e9cf5d9054a1c3a73
weishenggao <527518190@qq.com>
2022-08-12 17:39:10 +0800

    ci

--- src/script/sync.ts
+++ src/script/sync.ts
@@ -4,9 +4,9 @@ import Todo_List from '../model/todo_list'
 
 (async function () {
   try {
-    await Auth.sync({alter: true})
-    await User_Login.sync({alter: true})
-    await Todo_List.sync({alter: true})
+    await Auth.sync({ alter: true })
+    await User_Login.sync({ alter: true })
+    await Todo_List.sync({ alter: true })
   } catch (e) {
     console.log(e);
   } finally {
