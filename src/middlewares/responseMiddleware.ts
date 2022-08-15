ea1050424279336a0eab7dbfa6f0043d7d5dc9e8
weishenggao <527518190@qq.com>
2022-08-16 01:44:07 +0800

    body转成小驼峰

--- src/middlewares/responseMiddleware.ts
+++ src/middlewares/responseMiddleware.ts
@@ -1,18 +1,20 @@
-import { Context, Next } from 'koa';
-import { Middleware, KoaMiddlewareInterface } from 'routing-controllers';
-import { Service } from 'typedi';
+import { Context, Next } from "koa";
+import { Middleware, KoaMiddlewareInterface } from "routing-controllers";
+import { Service } from "typedi";
+import { toCamelCase } from "@/utils";
 
-@Middleware({ type: 'after' })
+@Middleware({ type: "after" })
 @Service()
 export default class ResponseMiddleware implements KoaMiddlewareInterface {
-    async use(ctx: Context, next: Next): Promise<any> {
-        await next()
-        ctx.status = ctx.status || 200
-        ctx.body = {
-            code: 0,
-            data: ctx.body,
-            message: ctx.message,
-            extra: {}
-        }
-    }
+  async use(ctx: Context, next: Next): Promise<any> {
+    await next();
+    ctx.status = ctx.status || 200;
+    const camelCaseBody = toCamelCase(ctx.body);
+    ctx.body = {
+      code: 0,
+      data: camelCaseBody,
+      message: ctx.message,
+      extra: {},
+    };
+  }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,26 @@
-export function toCamelCase(data) {
-    if (Array.isArray(data)) {
-        return data.map((ele) => {
-            const keys = Object.keys(ele);
+import { camelCase } from "lodash";
 
-        })
-    }
+export function isObject(data) {
+  return Object.prototype.toString.call(data) === "[object Object]";
+}
+
+export function isArray(data) {
+  return Array.isArray(data);
+}
+
+export function toCamelCase(data) {
+  if (isArray(data)) {
+    return data.map((ele) => {
+      return toCamelCase(ele);
+    });
+  } else if (isObject(data)) {
+    return Object.keys(data).reduce((info, ele) => {
+      const key = camelCase(ele);
+      const value = data[ele];
+      info[key] = toCamelCase(value);
+      return info;
+    }, {});
+  } else {
+    return data;
+  }
 }
